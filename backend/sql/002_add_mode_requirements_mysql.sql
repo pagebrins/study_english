021230b917b93b0ad45ca084eea22812995d3bf6
shan.wang <shan.wang@shopee.com>
2026-05-19 15:26:34 +0800

    init repo

--- backend/sql/002_add_mode_requirements_mysql.sql
+++
@@ -1,3 +0,0 @@
-USE study_english;
-
-ALTER TABLE modes ADD COLUMN IF NOT EXISTS requirements TEXT NULL AFTER mode;
--- backend/sql/009_add_theme_name_level_unique_mysql.sql
+++
@@ -1,6 +0,0 @@
-USE study_english;
-
-ALTER TABLE themes
-  DROP PRIMARY KEY,
-  ADD PRIMARY KEY (name, level),
-  ADD UNIQUE KEY uk_themes_id (id);
